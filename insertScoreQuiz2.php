288778d5126dbbf776c91e3eed2d29f2536aa169
Nurul Filzah <filzahn1@gmail.com>
2023-06-17 21:19:33 +0800

    test

--- insertScoreQuiz2.php
+++
@@ -1,6 +0,0 @@
-<?php 
-include "connection.php";
-// value '2' is the username id
-    $sqlInsert = "INSERT INTO `student_score2` (`student_id`, `quiz2`) VALUES ('2','{$_GET['score']}' );";
-    $result = $conn->query($sqlInsert);
-?>
--- insertScoreQuiz3.php
+++
@@ -1,5 +0,0 @@
-<?php 
-include "connection.php";
-    $sqlInsert = "INSERT INTO `student_score3` (`student_id`, `quiz3`) VALUES ('1','{$_GET['score']}' );";
-    $result = $conn->query($sqlInsert);
-?>
